b830d3242b4935ef3113f7341ab04d45d9a94c36
hongseok <hghg990124@naver.com>
2025-08-27 02:32:45 +0900

    Feat: 홈 기능 설명란 컴포넌트 생성

--- englishwords/src/feature/home/FeatureInfo.tsx
+++ englishwords/src/feature/home/FeatureInfo.tsx
@@ -1,16 +1,16 @@
 interface FeatureInfoProps {
   title: string,
   content: string,
-  image: string
+  image: "test" | "word" | "template"
 }
 
 const FeatureInfo: React.FC<FeatureInfoProps> = ({title, content, image}) => {
   return (
-    <div className="flex ">
+    <div className="flex w-[800px]">
       <img src={image} alt={`${image} 이미지`} />
 
-      <div>
-        <p>{title}</p>
+      <div className="flex flex-col gap -2">
+        <p className="font-bold text-4xl">{title}</p>
 
         <p>{content}</p>
       </div>
--- englishwords/src/feature/home/Home.tsx
+++ englishwords/src/feature/home/Home.tsx
@@ -1,4 +1,6 @@
 import HomeFeatureBox from "./HomeFeatureBox"
+import FeatureInfo from "./FeatureInfo"
+
 const Home: React.FC = () => {
   return(
     <div className=" mt-20 flex flex-col px-5">
@@ -13,6 +15,15 @@ const Home: React.FC = () => {
         <HomeFeatureBox title="모의 테스트" color="#FFE680" image="test"/>
         <HomeFeatureBox title="단어암기" color="#A7E6C2" image="word"/>
         <HomeFeatureBox title="단계별 탬플릿" color="#BFEFFF" image="template"/>
+
+      </div >
+
+      <div className="flex flex-col gap-2 mt-20">
+        <FeatureInfo title="모의 시험을 통해 실력을 확인해보세요" content="시험과 유사한 방식의 문제를 풀어보면서 문제점을 확인하고 시험에 익숙해져보세요" image="test"/>
+        <FeatureInfo title="단어의 뜻과 발음을 외워보세요" content="시험에서 자주 나오는 단어를 발음과 함께 외워보세요" image="word"/>
+        <FeatureInfo title="단계별에 맞는 템플릿을 확인해보세요" content="단계별 자주 사용되는 문장을 템플릿으로 만나보세요" image="template"/>
+      </div>
+      <div>
 
       </div>
     </div>

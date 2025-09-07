aae83337049f672942d94f8eb62165b28828055e
hongseok <hghg990124@naver.com>
2025-09-07 21:51:13 +0900

    Feat: 회원가입 및 db 연결

---
+++ backend/englishword.sql
@@ -0,0 +1,8 @@
+use sys;
+
+create table if not exists users (
+	id varchar(100) primary key comment "사용자 로그인 아이디",
+    name varchar(100) not null comment "사용자 이름",
+    birthday date not null comment "사용자 생년월일",
+    password varchar(300) not null comment "로그인 암호, 패스워드"
+);
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,51 @@
 import express, {Request, Response} from "express"
+import mysql from "mysql2/promise"
+import cors from "cors"
 
 const app = express()
+// const dbconfig = require("./config/dbconfig.json")
+
+// const pool = mysql.createPool({
+//   host: dbconfig.host,
+//   user: dbconfig.user,
+//   password: dbconfig.password,
+//   database: dbconfig.database,
+// })
+
+// async function connectDB() {
+//   try {
+//     const conn  = await pool.getConnection()
+//     console.log("DB connected")
+//     conn.release()  
+//   } catch (error) {
+//     console.error("DB connection error:", error)
+//   }
+// }
+// connectDB()
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}))
+
+
+
+app.get("/login", (req: Request, res: Response) => {
+  
+})
+
+app.post("/signup", (req: Request, res: Response) => {
+  const id = req.body.id
+  const password = req.body.password
+  const nickname = req.body.nickname
+  const birthday = req.body.birthday
+  console.log(id, password, nickname, birthday)
+})
 
 app.listen(8080, () => {
-  console.log("start server")
-})
+    console.log("start server")
+  })
